87c3d9c2169cbfaf48bd113c9a81755795ea4e13
ShuangYa <896640547@qq.com>
2019-04-21 22:31:48 +0800

    Add model example

--- app/Module/Index/Controller/Index.php
+++ app/Module/Index/Controller/Index.php
@@ -1,7 +1,16 @@
 <?php
-namespace YesfApp\Module\Index\Controller;
+namespace App\Module\Index\Controller;
+
+use App\Model\User;
 
 class Index {
+	private $user;
+
+	// DI Example
+	public function __construct(User $user) {
+		$this->user = $user;
+	}
+
 	public function IndexAction($request, $response) {
 		$response->assign('message', 'Hello, Yesf!');
 	}
